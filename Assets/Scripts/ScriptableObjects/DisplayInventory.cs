c4236bcd82511e19c4cad3162e4827d1f2387de4
wbvanderbroek <wbvanderbroek@st.noorderpoort.nl>
2024-02-19 16:22:31 +0100

    inventory really do be working

--- Assets/Scripts/ScriptableObjects/DisplayInventory.cs
+++ Assets/Scripts/ScriptableObjects/DisplayInventory.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
@@ -12,35 +13,29 @@ public class DisplayInventory : MonoBehaviour
     public int NumberOfColumn;
 
     Dictionary<InventorySlot, GameObject> itemsDisplayed = new Dictionary<InventorySlot, GameObject>();
-
     private void Start()
     {
-        // Set XStart and YStart to position the top-left corner of the grid
-
         CreateDisplay();
     }
-
     private void Update()
     {
-        UpdateDisplay();
+        UpdateDisplay(); 
     }
-
     public void CreateDisplay()
     {
         for (int i = 0; i < inventory.Container.Count; i++)
         {
-            var obj = Instantiate(inventory.Container[i].item.prefab, Vector3.zero, Quaternion.identity, transform);
+            var obj  = Instantiate(inventory.Container[i].item.prefab, Vector3.zero, Quaternion.identity, transform);
             obj.GetComponent<RectTransform>().anchoredPosition = GetPosition(i); // Use anchored position instead of local position
             obj.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
             itemsDisplayed.Add(inventory.Container[i], obj);
         }
     }
-
-    public Vector2 GetPosition(int i)
+    public Vector3 GetPosition(int i)
     {
         int column = i % NumberOfColumn;
         int row = i / NumberOfColumn;
-        return new Vector2(XStart + (XSpaceBetweenItem * column), YStart - (YSpaceBetweenItem * row)); // Use Vector2 for anchored position
+        return new Vector3(XStart + (XSpaceBetweenItem * column), YStart - (YSpaceBetweenItem * row), 0f);
     }
 
     public void UpdateDisplay()
