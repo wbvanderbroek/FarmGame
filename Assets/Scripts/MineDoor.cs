1d797af8d854132e6ab94a6bb659b413d33d083c
wbvanderbroek <wbvanderbroek@st.noorderpoort.nl>
2024-05-20 19:56:12 +0200

    crossfade animation added

--- Assets/Scripts/MineDoor.cs
+++ Assets/Scripts/MineDoor.cs
@@ -1,23 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MineDoor : MonoBehaviour
 {
+    [SerializeField] private Animator transition;
+    private void Start()
+    {
+        transition = GameObject.Find("CanvasCrossfade").GetComponent<Animator>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             other.gameObject.GetComponent<PlayerMovement>().canMove = false;
-            if (SceneManager.GetActiveScene().name == "Main")
-            {
-                SceneManager.LoadScene("Generation");
-            }
-            if (SceneManager.GetActiveScene().name == "Generation")
-            {
-                SceneManager.LoadScene("Main");
-            }
+            StartCoroutine(LoadScene());
+        }
+    }
+    private IEnumerator LoadScene()
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(2);
+        if (SceneManager.GetActiveScene().name == "Main")
+        {
+            SceneManager.LoadScene("Generation");
+        }
+        if (SceneManager.GetActiveScene().name == "Generation")
+        {
+            SceneManager.LoadScene("Main");
         }
     }
 }
